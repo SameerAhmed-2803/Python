48a6ef03aef2f1bfaf00a9c869e759ca1e1fba0c
sahmed3 <sahmed3@uno.edu>
2024-02-22 12:56:54 -0600

    Making my first GUI in Python

---
+++ Learning Tkinter/FirstGUI.py
@@ -0,0 +1,12 @@
+#importing tkinter library
+from tkinter import *
+#creating root window
+root = Tk()
+
+#root window title and dimensions
+root.title("Welcome to my first Python GUI")
+#geometry = (width*height)
+root.geometry('350x300')
+
+#executing Tkinter
+root.mainloop()

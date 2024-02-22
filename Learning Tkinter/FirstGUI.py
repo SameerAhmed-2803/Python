46a57733baaea9eec1723b3a83223e3402d7a612
sahmed3 <sahmed3@uno.edu>
2024-02-22 12:59:47 -0600

    Adding labels/text to the gui

--- Learning Tkinter/FirstGUI.py
+++ Learning Tkinter/FirstGUI.py
@@ -8,5 +8,8 @@ root.title("Welcome to my first Python GUI")
 #geometry = (width*height)
 root.geometry('350x300')
 
+label = Label(root, text="The first label to the first gui")
+label.grid()
+
 #executing Tkinter
 root.mainloop()
